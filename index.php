f69e46ee18dc1c963f123ddb7f806653238d8f01
Nichapha Khanaphiban <>
2023-12-18 19:40:16 +0700

    FF

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <html>
     <body>
-       <h1>This is PHP <?php  echo 'Hello World'?></h1> 
+       <h1>This is PdHP <?php  echo 'Hello World'?></h1> 
+       <div>
+        <p>1</p>
+        <?php
+        $var_a = 1;
+        $var_a = "1";
+        print_r($var_a);
+        var_dump($var_a);
+        echo $var_a + 2; //12,3
+        {
+             echo "<h1>my h1</h1>";
+                //$ การสร้างตัวแปล
+               
+       ?>
+            <p>2</p>
+        <?php
+        }
+        ?>
+            <p>3</p>
+        </div>
     </body>
 </html>
