d9401869db855db3ba509b2eab2a15b202065f7b
TeEnInDy <98180053+TeEnInDy@users.noreply.github.com>
2023-12-21 02:29:23 +0700

    Finish

--- PHP_01/PHP_01.php
+++ PHP_01/PHP_01.php
@@ -1,28 +1,239 @@
 <html>
     <body>
     <link rel="stylesheet" href="class.css">
-    <center> 
+    <link rel="stylesheet" href="scrollbar.css">
+    <link rel="stylesheet" href="sticky.css">  
+    <?php include ('cal.php');?>
+<div>
+<div class="container3"> 
+        <img src="https://media.giphy.com/media/duXouwPAEINMmFS6Xn/giphy.gif">
+        </div>
+        <div class="container4"> 
+        <img src="https://media.giphy.com/media/sCYjsNKCpiNlZ9jiuR/giphy.gif">
+        </div>
+    <center>
         <div class="box">
             <div class="fontchange">
-                <?php
-                $var_multiplier = 12;
-                $var_numberOfMultiples = 12;
-                echo "สูตรคูณแม่ $var_multiplier <br>";
-                for ($i = 1; $i <= $var_numberOfMultiples; $i++) { 
-                    $var_result = $var_multiplier * $i;
-                    echo "$var_multiplier x $i = $var_result<br>";
-                }
-                ?>
+                 <div> 
+                   <?php myMessage(1); ?>
+                </div>
+            </div>
+        </div>
+        </center> 
+        <div class="container1"> 
+        <img src="https://media.giphy.com/media/tJqyalvo9ahykfykAj/giphy.gif">
+        </div>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(2); ?>
+                </div>
             </div>
         </div>
         </center>
-        <div class="container1" > 
-        <img src="https://media.giphy.com/media/tJqyalvo9ahykfykAj/giphy.gif" alt="">
-        </div>
         <div class="container2" >
-            <img src="https://media.giphy.com/media/Q22kcRdASuBvW/giphy.gif" alt="">
+            <img src="https://media.giphy.com/media/Q22kcRdASuBvW/giphy.gif">
         </div>
-
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(3); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(4); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(5); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(6); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(7); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(8); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(9); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(10); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(11); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(12); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(13); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(14); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(15); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(16); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(17); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(18); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(19); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(20); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(21); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(22); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(23); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        <center>
+        <div class="box">
+            <div class="fontchange">
+                 <div> 
+                   <?php myMessage(24); ?>
+                </div>
+            </div>
+        </div>
+        </center>
+        </div>
         
     </body>
 </html>
---
+++ cal.php
@@ -0,0 +1,15 @@
+<?php
+function myMessage($var_multiplier) {
+                $var_multiplier;
+                $var_numberOfMultiples = 12;
+                echo "สูตรคูณแม่ $var_multiplier <br>";
+                for ($i = 1; $i <= $var_numberOfMultiples; $i++) { 
+                    $var_result = $var_multiplier * $i;
+                    echo "$var_multiplier x $i = $var_result<br>";
+                }
+            }
+                ?>
+ 
+ 
+
+ 
